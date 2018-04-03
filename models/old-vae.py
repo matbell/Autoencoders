ecf91243465f472da68b6314d2288961d57c6d7b
mattia <mattia@mcampana.iit.cnr.it>
2018-04-03 16:42:19 +0200

    .

--- models/old-vae.py
+++ models/old-vae.py
@@ -1,13 +1,11 @@
-'''
-Code source: https://github.com/keras-team/keras/blob/master/examples/variational_autoencoder.py
-
+'''This script demonstrates how to build a variational autoencoder with Keras.
  #Reference
+
+ https://github.com/keras-team/keras/blob/master/examples/variational_autoencoder.py
 
  - Auto-Encoding Variational Bayes
    https://arxiv.org/abs/1312.6114
 '''
-from __future__ import print_function
-
 from keras.layers import Input, Dense, Lambda
 from keras.models import Model
 from keras import backend as K
@@ -33,21 +31,25 @@ class Vae:
         self.decoder_h = None
         self.decoder_mean = None
 
-        K.clear_session()
-
         self.build_model()
 
     def sampling(self, args):
         z_mean, z_log_var = args
-        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], self.latent_dim), mean=0., stddev=self.epsilon_std)
+        epsilon = K.random_normal(shape=(self.batch_size, self.latent_dim), mean=0., stddev=self.epsilon_std)
         return z_mean + K.exp(z_log_var / 2) * epsilon
+
+    def vae_loss(self, x, x_decoded_mean):
+        xent_loss = self.original_dim * metrics.binary_crossentropy(x, x_decoded_mean)
+        kl_loss = - 0.5 * K.sum(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
+        return xent_loss + kl_loss
 
     def build_model(self):
 
-        self.x = Input(shape=(self.original_dim,))
+        self.x = Input(batch_shape=(self.batch_size, self.original_dim))
         h = Dense(self.intermediate_dim, activation='relu')(self.x)
         self.z_mean = Dense(self.latent_dim)(h)
         self.z_log_var = Dense(self.latent_dim)(h)
+
 
         # note that "output_shape" isn't necessary with the TensorFlow backend
         z = Lambda(self.sampling, output_shape=(self.latent_dim,))([self.z_mean, self.z_log_var])
@@ -61,30 +63,27 @@ class Vae:
         # instantiate VAE model
         self.vae = Model(self.x, x_decoded_mean)
 
-        # Compute VAE loss
-        xent_loss = self.original_dim * metrics.binary_crossentropy(self.x, x_decoded_mean)
-        kl_loss = - 0.5 * K.sum(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
-        vae_loss = K.mean(xent_loss + kl_loss)
-
-        self.vae.add_loss(vae_loss)
-        self.vae.compile(optimizer='rmsprop')
+        self.vae.compile(optimizer='rmsprop', loss=self.vae_loss)
         self.vae.summary()
 
     def fit(self, x_train, x_test):
 
-        self.vae.fit(x_train,
+        # Train the entire model
+        self.vae.fit(x_train, x_train,
                      shuffle=True,
                      epochs=self.epochs,
                      batch_size=self.batch_size,
-                     validation_data=(x_test, None))
+                     validation_data=(x_test, x_test))
 
     def encode(self, data):
+
         # build a model to project inputs on the latent space
         self.encoder = Model(self.x, self.z_mean)
 
         return self.encoder.predict(data, batch_size=self.batch_size)
 
     def decode(self, data):
+
         # build a data generator that can sample from the learned distribution
         decoder_input = Input(shape=(self.latent_dim,))
         _h_decoded = self.decoder_h(decoder_input)
